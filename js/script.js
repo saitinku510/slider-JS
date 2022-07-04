e7674aa4d68ff8cc212a6a737b3a80300921ccf2
venkatasai510 <venkatasai@sketchbrahma.com>
2022-07-04 09:36:17 +0530

    setInterval

--- js/script.js
+++ js/script.js
@@ -5,6 +5,46 @@ let tabContentOne = document.getElementById('tabContent1');
 let tabContentTwo = document.getElementById('tabContent2');
 let tabContentThree = document.getElementById('tabContent3');
 
+
+setInterval(() => {
+    tabTitleOne.classList.remove('active');
+}, 10000);
+setInterval(() => {
+    tabTitleTwo.classList.add('active');
+}, 10000);
+setInterval(() => {
+    tabTitleTwo.classList.remove('active');
+}, 20000);
+setInterval(() => {
+    tabTitleThree.classList.add('active');
+}, 20000);
+setInterval(() => {
+    tabTitleThree.classList.remove('active');
+}, 30000);
+setInterval(() => {
+    tabTitleOne.classList.add('active');
+}, 30000);
+
+setInterval(() => {
+    tabContentOne.classList.remove('active');
+}, 10000);
+setInterval(() => {
+    tabContentTwo.classList.add('active');
+}, 10000);
+setInterval(() => {
+    tabContentTwo.classList.remove('active');
+}, 20000);
+setInterval(() => {
+    tabContentThree.classList.add('active');
+}, 20000);
+setInterval(() => {
+    tabContentThree.classList.remove('active');
+}, 30000);
+setInterval(() => {
+    tabContentOne.classList.add('active');
+}, 30000);
+
+
 tabTitleOne.addEventListener('click', (e) => {
     tabTitleOne.classList.add('active');
     tabTitleTwo.classList.remove('active');
